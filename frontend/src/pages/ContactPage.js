779e4ee054626efe5569db51149a31904eec2fc3
Stefi <stefi.kovacs93@gmail.com>
2023-01-24 15:32:05 +0100

    fetch all contacts

--- frontend/src/pages/ContactPage.js
+++ frontend/src/pages/ContactPage.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function ContactPage() {
-  return <div>Contact</div>;
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    const fetchContacts = async() => {
+      const response = await fetch(
+        `${process.env.REACT_APP_URL}/api/contacts`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+        }
+      );
+      const data = await response.json();
+      if(response.ok){
+        setContacts(data.data)
+      }
+    }
+    fetchContacts()
+  }, [])
+  return (<div>
+    <div>
+      {contacts && contacts.map((contact) => (
+        <p key={contact.id} >{contact.name}</p>
+      ))}
+    </div>
+  </div>);
 }
